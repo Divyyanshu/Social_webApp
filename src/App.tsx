e9dc2928d56470608c63b0a100a426942c2cbcc4
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-07-03 22:38:44 +0530

    setup nextui for social media app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@nextui-org/button'
+
+function App() {
+  return (
+    <>
+    <button className='bg-stone-800 text-cyan-200'>Basic</button>
+      <Button color='primary' className='bg-zinc-600'>Click</Button>
+    </>
+  )
+}
+
+export default App
