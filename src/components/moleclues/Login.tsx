d30da8a715b5350b6c3c8647463d0a0d5413bae0
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-07-06 19:03:30 +0530

    login and register

--- src/components/moleclues/Login.tsx
+++ src/components/moleclues/Login.tsx
@@ -1,21 +1,33 @@
 import { Button } from "@nextui-org/button";
 import { Card, CardHeader } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
+import { useForm } from "react-hook-form";
+
+interface LoginFormInterface {
+ email : string,
+ password : string,
+}
+
 
 const Login = () => {
+  const { register, handleSubmit } = useForm<LoginFormInterface>();
+  async function onSubmit(values:LoginFormInterface) {
+    console.log(values.email,values.password)
+  }
   return (
     <div className="flex h-screen justify-center items-center">
       <Card className="p-8 w-96 flex flex-col gap-4">
         <CardHeader>
          <h1 className="text-2xl text-violet-600 font-bold ">Login Here</h1>
         </CardHeader>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
         <Input
          isRequired
           type="email"
           label="Email"
           defaultValue=""
           className="max-w-xs"
+          {...register("email")}
         />
         <Input
          isRequired
@@ -23,6 +35,7 @@ const Login = () => {
           label="Password"
           defaultValue=""
           className="max-w-xs"
+          {...register("password")}
         />
         <Button type = "submit" color = "secondary">Login</Button>
         </form>
